b46913746afbbabc93ee58bc7822dcfa47971711
AnthonyFerry <a.ferry@ludus-academie.fr>
2017-03-15 14:46:26 +0100

    SQLiteOpenHelper Checkpoint

--- app/src/main/java/com/example/anthonyferry/appmobile_android_bdd_anthony_ferry/ChapitreBaseSQLite.java
+++ app/src/main/java/com/example/anthonyferry/appmobile_android_bdd_anthony_ferry/ChapitreBaseSQLite.java
@@ -1,5 +1,6 @@
 package com.example.anthonyferry.appmobile_android_bdd_anthony_ferry;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,18 +10,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ChapitreBaseSQLite extends SQLiteOpenHelper {
 
-    public static final String nom_Table;
-    public static final String Ma_Colonne_Id;
-    public static final String Ma_Colonne_Nom;
-    public static final String Ma_Colonne_Description;
+    public static final String nom_Table = "CHAPITRE";
+    public static final String Ma_Colonne_Id = "ID";
+    public static final String Ma_Colonne_Nom = "NOM";
+    public static final String Ma_Colonne_Description = "DESCRIPTION";
+
+    public static final String CREATE_BDD = "CREATE TABLE " + nom_Table + "(" + Ma_Colonne_Id + " PRIMARY KEY AUTOINCREMENT, " + Ma_Colonne_Nom + " VARCHAR(32) NOT NULL, " + Ma_Colonne_Description + " TEXT NOT NULL);";
+
+    public ChapitreBaseSQLite(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super (context, name, factory, version);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_BDD);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE " + nom_Table);
+        onCreate(db);
     }
 }
