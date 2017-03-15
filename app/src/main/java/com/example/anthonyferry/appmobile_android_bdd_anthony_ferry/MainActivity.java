a70ae75acfec5a681ddc7f647d3c42086f2fb93b
AnthonyFerry <a.ferry@ludus-academie.fr>
2017-03-15 15:59:11 +0100

    Add main activity layout and java class

--- app/src/main/java/com/example/anthonyferry/appmobile_android_bdd_anthony_ferry/MainActivity.java
+++ app/src/main/java/com/example/anthonyferry/appmobile_android_bdd_anthony_ferry/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.anthonyferry.appmobile_android_bdd_anthony_ferry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdd, btnRemove, btnSearch;
+    private ListView listChapitre;
+
+    private ChapitreBDD bdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GetObjectsId();
+
+        bdd = new ChapitreBDD(this);
+
+        ArrayList<Chapitre> chapitres = bdd.getAllChapters();
+
+        ArrayAdapter<Chapitre> adapter = new ArrayAdapter<Chapitre>(this, android.R.layout.simple_list_item_1, chapitres);
+
+        listChapitre.setAdapter(adapter);
+    }
+
+    private void GetObjectsId()
+    {
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnRemove = (Button) findViewById(R.id.btnRemove);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+
+        listChapitre = (ListView) findViewById(R.id.listChapitres);
     }
 }
